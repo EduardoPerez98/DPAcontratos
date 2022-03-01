f510b2f2950a8aee79e63699c2a46e9606eaa73d
EduardoPerez98 <98420019+EduardoPerez98@users.noreply.github.com>
2022-03-01 15:56:40 -0600

    Add files via upload

---
+++ DPA/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../DPA/css/normalize.css">
+</head>
+<body>
+<header> 
+    <div>
+        <h1>Universidad Autonoma de Chiapas</h1>
+    </div>
+    <div>
+        <nav>
+            <ul>
+                <li><a href="">Dependencias</a></li>
+                <li><a href="">Ver contratos</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<div>
+    <h3>CONTRATOS</h3>
+    <div>
+        <h4>Tiempo Determinado</h4>
+        <a href="web/cont_htd.php">Empezar</a>
+    </div>
+    <div>
+        <h4>Tiempo Determinado para cubrir Interinato</h4>
+        <a href="web/cont_htdi.php">Empezar</a>
+    </div>
+</div>
+
+
+<footer>
+
+</footer>
+
+</body>
+</html>
+
+

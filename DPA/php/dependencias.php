0c2e15d010d7db37868b8359a2786e7adced0bd7
EduardoPerez98 <98420019+EduardoPerez98@users.noreply.github.com>
2022-03-02 09:41:31 -0600

    Add files via upload

---
+++ DPA/php/dependencias.php
@@ -0,0 +1,22 @@
+<?php 
+require '../settings/conexion.php';
+
+$nom_depe = $_POST['nom_depe'];
+$nom_enca = $_POST['nom_enca'];
+$puesto_cargo = $_POST['puesto_cargo'];
+$fecha_cargo = $_POST['fecha_cargo'];
+$ciclo_ini = $_POST['ciclo_ini'];
+$ciclo_fin = $_POST['ciclo_fin'];
+
+
+
+$ins = pg_query($conn ,"INSERT INTO dependencias VALUES
+ ('1','$nom_depe', '$nom_enca', '$puesto_cargo', '$fecha_cargo', '$ciclo_ini', '$ciclo_fin') ");
+
+if($ins)
+    echo "inserción completa";
+else{
+    echo "Ocurrió un error".pg_last_error();
+}
+
+?>
